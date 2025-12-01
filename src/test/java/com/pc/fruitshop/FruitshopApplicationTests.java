c29ee65b0f67c39fa09dcd397bac46d6bbe289d0
P.C.Lu <lupc9405@gmail.com>
2025-12-01 11:23:20 +0800

    Init project

---
+++ src/test/java/com/pc/fruitshop/FruitshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc.fruitshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
